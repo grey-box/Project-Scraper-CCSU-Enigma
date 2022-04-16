852bddd207ca6e936ce85015217c30fdfbdca37d
Kyle Guarco <kyleguarco55@gmail.com>
2022-04-15 20:45:27 -0400

    Testing CORS proxies (Rob--W/cors-anywhere)
    cors-anywhere must be installed and running on localhost port 8080

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
-
-
-
+chrome.runtime.onInstalled.addListener(() => {
+	chrome.contextMenus.create({
+		"id": "bliss-web-scraper",
+		"title": "Greybox Web Scraper",
+		"contexts": ["page", "link"]
+	})
+})
--- popup.js
+++ popup.js
@@ -1,7 +1,20 @@
+let form = document.getElementById("url-form")
 
+form.addEventListener('submit', async (event) => {
+	let url = form.elements["url"]
 
-chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-  console.log(tabs[0].url);
-  const urlForm = document.getElementById('urlFormInput');
-  urlForm.value = tabs[0].url;
-});
+	fetch('http://127.0.0.1:8080/' + url.value)
+		.then(res => res.text())
+		.then(txt => { console.log(txt) })
+		.catch(e => {
+			console.log(e)
+		})
+
+	await chrome.downloads.download({
+		url: 'http://127.0.0.1:8080/' + url.value,
+		filename: 'cors_file'
+	})
+
+	event.preventDefault()
+})
+
